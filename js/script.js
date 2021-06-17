9b1a0cfe871f969b93c7c8d6245208fb90bfd191
Daniil Veselov <daniil.veselov2012@yandex.ru>
2021-06-17 16:27:35 +0300

    lesson17.2

--- js/script.js
+++ js/script.js
@@ -3,22 +3,41 @@
 window.addEventListener('DOMContentLoaded', function () {
    'use strict';
 
-   const input = document.querySelector('#input'),
-      parag = document.querySelector('#parag');
-   let text,
-      timer;
+   const btnPlay = document.querySelector('#btnPlay'),
+      car = document.querySelector('.car'),
+      btnReset = document.querySelector('#btnReset');
+   let count = 0,
+      width = window.innerWidth - 100,
+      userWidth,
+      rideInterval;
 
-   function vivod(value) {
-      parag.textContent = value;
+   let animate = function () {
+      rideInterval = requestAnimationFrame(animate);
+      count++;
+
+      if (count < width) {
+         car.style.left = count + 'px';
+      } else {
+         cancelAnimationFrame(rideInterval);
+      }
+   };
+
+   function reset() {
+      cancelAnimationFrame(rideInterval);
+      clearInterval(userWidth);
+      count = 0;
+      car.style.left = count + 'px';
    }
 
+   function findWidth() {
+      width = window.innerWidth - 100;
+   }
 
-   input.addEventListener('input', () => {
-      clearTimeout(timer);
-
-      text = input.value;
-
-      timer = setTimeout(vivod, 300, text);
+   btnPlay.addEventListener('click', () => {
+      rideInterval = requestAnimationFrame(animate);
+      userWidth = setInterval(findWidth, 1000);
    });
+
+   btnReset.addEventListener('click', reset);
 
 });
