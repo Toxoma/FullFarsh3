e4f033ca9c21e7a305501353d1ee622232baeeed
Daniil Veselov <daniil.veselov2012@yandex.ru>
2021-06-18 19:17:31 +0300

    lesson20;

--- js/script.js
+++ js/script.js
@@ -1,43 +1,110 @@
 /* eslint-disable */
+"use strict";
 
-window.addEventListener('DOMContentLoaded', function () {
-   'use strict';
+class Todo {
+   constructor(form,input,todoList,todoCopmpleted,todoContainer) {
+      this.form = document.querySelector(form);
+      this.input = document.querySelector(input);
+      this.todoList = document.querySelector(todoList);
+      this.todoCopmpleted = document.querySelector(todoCopmpleted);
+      this.todoContainer = document.querySelector(todoContainer);
 
-   const btnPlay = document.querySelector('#btnPlay'),
-      car = document.querySelector('.car'),
-      btnReset = document.querySelector('#btnReset');
-   let count = 0,
-      width = window.innerWidth - 100,
-      userWidth,
-      rideInterval;
+      this.todoData = new Map(JSON.parse(localStorage.getItem('toDoList')));
+   }
 
-   let animate = function () {
-      rideInterval = requestAnimationFrame(animate);
-      count++;
+   addToStorage() {
+      localStorage.setItem('toDoList', JSON.stringify([...this.todoData]));
+   }
 
-      if (count < width) {
-         car.style.left = count + 'px';
+   addTodo(e) {
+      e.preventDefault();
+      
+      if (this.input.value.trim()) {
+         const newTodo = {
+            value: this.input.value,
+            completed: false,
+            key: this.generateKey(),
+         };
+         this.todoData.set(newTodo.key, newTodo);
+         this.render();
       } else {
-         cancelAnimationFrame(rideInterval);
+         alert('Пустое дело добавить нельзя!');
       }
-   };
-
-   function reset() {
-      cancelAnimationFrame(rideInterval);
-      clearInterval(userWidth);
-      count = 0;
-      car.style.left = count + 'px';
    }
 
-   function findWidth() {
-      width = window.innerWidth - 100;
+   render() {
+      this.todoList.textContent = '';
+      this.todoCopmpleted.textContent = '';
+      this.todoData.forEach(this.createItem,this);
+      this.addToStorage();
    }
 
-   btnPlay.addEventListener('click', () => {
-      rideInterval = requestAnimationFrame(animate);
-      userWidth = setInterval(findWidth, 1000);
-   });
+   createItem(item) {
+      const li = document.createElement('li');
+      li.classList.add('todo-item');
+      li.key = item.key;
+      li.insertAdjacentHTML('beforeend', `
+         <span class="text-todo">${item.value}</span>
+         <div class="todo-buttons">
+            <button class="todo-remove"></button>
+            <button class="todo-complete"></button>
+         </div>`);
 
-   btnReset.addEventListener('click', reset);
+      if (item.completed) {
+         this.todoCopmpleted.append(li);
+      } else {
+         this.todoList.append(li);
+      }
+   }
 
-});
+   generateKey() {
+      return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+   }
+
+   deleteItem(key) {
+      this.todoData.delete(key);
+      this.render();
+   }
+
+   completeItem(key) {
+      this.todoData.forEach(elem => {
+         if (elem.key===key) {
+            elem.completed = !elem.completed;
+         }
+      });
+      
+      this.render();
+   }
+
+   findLiKey(e) {
+      let target = e.target;
+      let liTargetKey;
+
+      switch (target.classList.value) {
+         case 'todo-remove':
+            liTargetKey = e.target.offsetParent.offsetParent.key;
+            this.deleteItem(liTargetKey);
+            break;
+         case 'todo-complete':
+            liTargetKey = e.target.offsetParent.offsetParent.key;
+            this.completeItem(liTargetKey);
+            break;
+      }
+   }
+   
+
+   handler() {
+      this.todoContainer.addEventListener('click', this.findLiKey.bind(this));
+   }
+
+   init() {
+      this.form.addEventListener('submit', this.addTodo.bind(this));
+      this.render();
+      this.handler();
+   }
+
+}
+
+const todo = new Todo('.todo-control','.header-input','.todo-list','.todo-completed','.todo-container');
+
+todo.init();
