04d110050106e628d2e3a264e5f8b94f504347dc
Daniil Veselov <daniil.veselov2012@yandex.ru>
2021-06-19 10:06:35 +0300

    hard

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@
 "use strict";
 
 class Todo {
-   constructor(form,input,todoList,todoCopmpleted,todoContainer) {
+   constructor(form,input,todoList,todoCopmpleted,todoContainer,todoEdit) {
       this.form = document.querySelector(form);
       this.input = document.querySelector(input);
       this.todoList = document.querySelector(todoList);
       this.todoCopmpleted = document.querySelector(todoCopmpleted);
       this.todoContainer = document.querySelector(todoContainer);
-
+      this.containerItems='';
+      this.todoEdit = document.querySelector(todoEdit);
+      this.deleteTimer ='';
       this.todoData = new Map(JSON.parse(localStorage.getItem('toDoList')));
    }
 
@@ -35,20 +37,25 @@ class Todo {
    render() {
       this.todoList.textContent = '';
       this.todoCopmpleted.textContent = '';
-      this.todoData.forEach(this.createItem,this);
+      this.todoData.forEach(this.createItem, this);
+      this.containerItems = this.todoContainer.querySelectorAll('li');
       this.addToStorage();
    }
 
    createItem(item) {
       const li = document.createElement('li');
+      li.style.transition = '1s';
       li.classList.add('todo-item');
       li.key = item.key;
       li.insertAdjacentHTML('beforeend', `
          <span class="text-todo">${item.value}</span>
          <div class="todo-buttons">
+            <button class="todo-edit"></button>
             <button class="todo-remove"></button>
             <button class="todo-complete"></button>
          </div>`);
+
+      li.style.transition = '1s';
 
       if (item.completed) {
          this.todoCopmpleted.append(li);
@@ -62,14 +69,83 @@ class Todo {
    }
 
    deleteItem(key) {
+      this.todoData.forEach(elem => {
+         if (elem.key===key) {
+            let li = Array.from(this.containerItems).find(item => item.key === key);
+            
+            li.style.transform = 'translateX(-110%)';
+         }
+      });
+
       this.todoData.delete(key);
-      this.render();
+      
+      if (this.deleteTimer==='') {
+         console.log('!');
+         this.deleteTimer = setTimeout(() => {
+            this.render();
+         }, 1000);
+      } else {
+         console.log('!!!');
+         clearTimeout(this.deleteTimer);
+         this.deleteTimer = '';
+         this.deleteTimer = setTimeout(() => {
+            this.render();
+      }, 1000);
+      }
+   }
+
+   transition(li) {
+      console.dir(li);
+
+      this.todoData.forEach(elem => {
+         if (elem.key === li.key) {
+
+            if (elem.completed) {
+               this.todoCopmpleted.append(li);
+               li.style.transform = 'translateX(-110%)';
+            } else {
+               this.todoList.append(li);
+               li.style.transform = 'translateX(-110%)';
+            }
+            setTimeout(() => {
+               li.style.transform = 'translateX(0%)';
+               this.addToStorage();
+            }, 100);
+         }
+      });
+
+
    }
 
    completeItem(key) {
       this.todoData.forEach(elem => {
-         if (elem.key===key) {
+         if (elem.key === key) {
             elem.completed = !elem.completed;
+
+            let li = Array.from(this.containerItems).find(item => item.key === key);
+            
+            li.style.transform = 'translateX(-110%)';
+
+            setTimeout(() => {
+               this.transition(li);
+            }, 1000);
+         }
+      });
+      
+
+   }
+
+   rewriteItem(key) {
+      this.todoData.forEach(elem => {
+         if (elem.key === key) {
+            let str;
+            do {
+               str = prompt('введите текст');
+               if (str===null) {
+                  return;
+               }
+            } while (!str.trim());
+            elem.value = str;
          }
       });
       
@@ -89,6 +165,10 @@ class Todo {
             liTargetKey = e.target.offsetParent.offsetParent.key;
             this.completeItem(liTargetKey);
             break;
+         case 'todo-edit':
+            liTargetKey = e.target.offsetParent.offsetParent.key;
+            this.rewriteItem(liTargetKey);
+            break;
       }
    }
    
@@ -105,6 +185,6 @@ class Todo {
 
 }
 
-const todo = new Todo('.todo-control','.header-input','.todo-list','.todo-completed','.todo-container');
+const todo = new Todo('.todo-control','.header-input','.todo-list','.todo-completed','.todo-container','.todo-edit');
 
 todo.init();
